71c198379991bfb8e0eb53117b25a8d649dbcb7d
Luca Urbanowicz <human.exp11@gmail.com>
2022-01-17 22:14:55 -0700

    added query selectors and event listeners

--- assets/script.js
+++ assets/script.js
@@ -11,50 +11,67 @@
 // THEN I can save my initials and my score
 
 
-var startButton = document.querySelector("#startQuiz");
-var count = 0;
+
+var numbOfQuestions = 8;
 var maxTime = 60;
-var points = 0;
-var nextButton =   ;
+var userScore = [ ];
+
 
 
 var questions = [ 
     {
         asked: "Which structure of code allows you to repeat the same action or actions a specified number of times?",
         choices: ["repeat", "for loop", "do while", "do loop"],
-        answer: "alert"
+        answer: "for loop"
     },
     {
         asked: "Which scope is at the top level and applies to all code below?",
         choices: ["National", "Wordly", "Global", "Above All"],
-        answer: "alert"
+        answer: "Global"
+    },
+    {
+        asked: "Which method joins several arrays and returns a new one?",
+        choices: ["filter", "replace", "push", "concat"],
+        answer: "concat"
+    },
+    {
+        asked: "Which method adds a new element at the end of an array?",
+        choices: ["appendChild", "push", "map", "sort"],
+        answer: "push"
+    },
+    {
+        asked: "Which function open up 'OK/Cancel' within the browser window and returns a boolean?",
+        choices: ["alert", "prompt", "confirm", "notify"],
+        answer: "confirm"
+    },
+    {
+        asked: "Name the function that will break up a string and return an integer.",
+        choices: ["breakInt", "parseInt", "number", "sliceInt"],
+        answer: "parseInt"
+    },
+    {
+        asked: "What is DOM short for?",
+        choices: ["Dungeon of Magicians", "Document of Methods", "Document Object Model", "Division of Methods"],
+        answer: "Document Object Model"
+    },
+    {
+        asked: "Which method adds list items to the end of existing lists that are under the same parent?",
+        choices: ["appendChild", "addChild", "appendParent", "appendCousin"],
+        answer: "appendChild"
     },
 ];
 
- 
-// Method that joins several arrays and returns a new array? A= concat
-
-// Method that adds a new element at the end of an array? A = push
-
-// Function that opens up "OK/Cancel" within browser window & returns T/F? A= Confirm ()
-
-// Which funtion will break up a string and return an integer? A= parseInt
-
-// What is "DOM" short for?  A=Document Object Model
-
-// Which method adds list items to the end of existing items under the same parent? A= appendChild
-// 
-
-
 // The init function is called when the page loads 
 function init() {
-    highScores();
+    var viewHighScores = JSON.parse(localStorage.getItem("userScore"));
+    if (viewHighScores !== null) {
+        userScore = viewHighScores;
+    }
 }
 
+function startQuiz() {
 
-
-
-
+}
 
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function beginTimer() {
@@ -80,21 +97,25 @@ function beginTimer() {
 }
 
 
-  // These functions are used by init
-function highScores() {
-    // Get stored value from client storage, if it exists
-    var storedResults = localStorage.getItem("userScores");
-    // If stored value doesn't exist, set counter to 0
-    if (storedResults === null) {
-      winCounter = 0;
-    } else {
-      // If a value is retrieved from client storage set the winCounter to that value
-      winCounter = storedResults;
-    }
-    //Render win count to page
-    score.textContent = winCounter;
-  }
+ 
+function restartQuiz() {
+ 
+}
 
-  init();
+function submitScore() {
 
+}
+
+
+var startButton = document.querySelector("#startQuiz");
+var submitScoreButton = document.querySelector("#submitScore");
+var restartButton = document.querySelector("#restartQuiz");
+var highscoresButton = document.querySelector("#viewHighscores");
+
+startButton.addEventListener("click", startQuiz);
+submitScoreButton.addEventListener("click", submitScore);
+highscoresButton.addEventListener("click", viewHighScores);
+init();
+
+restartButton.addEventListener("click", restartQuiz);
 
