48f97e312b77dafaf85850bbeda67b7f2c62a589
Luca Urbanowicz <human.exp11@gmail.com>
2022-01-21 11:31:54 -0700

    addended CSS and added for loop to JS

--- assets/script.js
+++ assets/script.js
@@ -18,59 +18,71 @@ var quizQuestionsEl = document.querySelector("#quizQuestions");
 var questions = [ 
     {
         asked: "Which structure of code allows you to repeat the same action or actions a specified number of times?",
-        choices: ["repeat", "for loop", "do while", "do loop"],
-        answer: "for loop"
+        choices: [ "repeat" , "for loop",   "do while", "do loop"],
+        answerIndex: 1,
     },
     {
         asked: "Which scope is at the top level and applies to all code below?",
-        choices: ["National", "Wordly", "Global", "Above All"],
-        answer: "Global"
+        choices: ["National", "Worldly",  "Global", "Above All"],
+        answerIndex: 2,
     },
     {
         asked: "Which method joins several arrays and returns a new one?",
         choices: ["filter", "replace", "push", "concat"],
-        answer: "concat"
+        answerIndex: 3,
     },
     {
         asked: "Which method adds a new element at the end of an array?",
         choices: ["appendChild", "push", "map", "sort"],
-        answer: "push"
+        answerIndex: 1,
     },
     {
         asked: "Which function open up 'OK/Cancel' within the browser window and returns a boolean?",
         choices: ["alert", "prompt", "confirm", "notify"],
-        answer: "confirm"
+        answerIndex: 2,
     },
     {
         asked: "Name the function that will break up a string and return an integer.",
         choices: ["breakInt", "parseInt", "number", "sliceInt"],
-        answer: "parseInt"
+        answerIndex: 1,
     },
     {
         asked: "What is DOM short for?",
         choices: ["Dungeon of Magicians", "Document of Methods", "Document Object Model", "Division of Methods"],
-        answer: "Document Object Model"
+        answerIndex: 2,
     },
     {
         asked: "Which method adds list items to the end of existing lists that are under the same parent?",
         choices: ["appendChild", "addChild", "appendParent", "appendCousin"],
-        answer: "appendChild"
+        answerIndex: 0,
     },
 ];
-
+var userAnswer;
+var choicesEl = document.querySelector("#choices");
 var numbOfQuestion = 0;
-var maxTime = 60;
-var timeRem = 0;
-var timer;
-var timerElement = document.getElementById("timer-count");
 var userScore = 0;
+var enterInititalsEl = document.querySelector("#initials")
 var highScoresButton = document.querySelector(".viewHighScores");
+
+var finalQIndex = questions.length;
+var currentQIndex = 0;
+var userScore = [ ];
+var highScores = [ ];
+var numbOfQuestion = 0;
+var timeRem = 60;
+var timer;
+var correct;
+var timerElement = document.getElementById("timer-count");
+var endGame;
+var highScoresButton = document.querySelector("viewHighScores");
+var startQuizButton = document.querySelector("startButton");
 // var listOfHighScores = [ ]
 
 
 
+function init( ) {}
 highScoresButton.addEventListener("click", function () {
-    document.getElementById("viewHighScores");
+    document.getElementById("#viewHighScores");
     var viewHighScores = JSON.parse(localStorage.getItem("userScore"));
     if (viewHighScores !== null) {
         userScore = viewHighScores;
@@ -78,13 +90,11 @@ highScoresButton.addEventListener("click", function () {
 });
 
 
-// // The init function is called when the page loads 
-// function init() {
-//     var viewHighScores = JSON.parse(localStorage.getItem("userScore"));
-//     if (viewHighScores !== null) {
-//         userScore = viewHighScores;
-//     }
-// }
+startQuizButton.addEventListener("click", function( ) {
+    document.getElementById("#startQuiz");
+    startQuiz();
+})
+
 
 function startQuiz() {
     beginTimer ();
@@ -93,16 +103,19 @@ function startQuiz() {
     
     quizQuestionsEl.classList.remove("hide");
     displayQuestions();
-
 }
+
 function displayQuestions() {
     var questionDiv = document.createElement("div");
     questionDiv.textContent = questions [numbOfQuestion].asked;
     quizQuestionsEl.append(questionDiv);
 
     // create buttons and append them here
-}
+    let choiceBtns = document.createElement("button");
+    button.innerHTML= choicesEl;
+    document.body.appendChild(choiceBtns);
 
+}
 
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function beginTimer() {
@@ -120,6 +133,30 @@ function beginTimer() {
     }, 1000);
 }
 
+function questions() {
+    for (i = 0; i < questions.length; i++) {
+        if (choiceBtns = asked.answerIndex) {
+            alert = ("Excellent!");
+        } else {
+            alert = ("Incorrect.");
+            timeRem = -5;
+        }
+        return;
+    };
+}
+
+    
+
+function endOfGame ( ) {
+    clearInterval(timer);
+    userScore = timeRem;
+    var initialsContainerEl = document.querySelector("initialsContainer");
+    initialsContainerEl.classList.remove("hide");
+}
+
+
+
+
 
  
 function restartQuiz() {
@@ -134,14 +171,14 @@ function submitScore() {
 }
 
 
-var startButton = document.querySelector("#startQuiz");
-// var submitScoreButton = document.querySelector("#submitScore");
-var restartButton = document.querySelector("#restartQuiz");
+// // var startButton = document.querySelector("#startQuiz");
+var submitScoreButton = document.querySelector("#submitScore");
+// // var restartButton = document.querySelector("#restartQuiz");
 
 
 startButton.addEventListener("click", startQuiz);
-// submitScoreButton.addEventListener("click", submitScore);
-// highScoresButton.addEventListener("click", viewHighScores);
-// restartButton.addEventListener("click", restartQuiz);
+// // // submitScoreButton.addEventListener("click", submitScore);
+// // // highScoresButton.addEventListener("click", viewHighScores);
+// // // restartButton.addEventListener("click", restartQuiz);
 
-// init();
+ init();
